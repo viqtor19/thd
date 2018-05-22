089641978602dc14cc5a3c8008f72e88f37c08a1
viqtor19 <39535889+viqtor19@users.noreply.github.com>
2018-05-23 00:13:35 +0200

    Add files via upload

---
+++ tree-tagger-polish-gate.bash
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+OPTIONS="-token -lemma -sgml"
+
+BIN=$SCRIPT_DIR/../../../../../treetagger/bin
+CMD=$SCRIPT_DIR/../../../../../treetagger/cmd
+LIB=$SCRIPT_DIR/../../../../../treetagger/lib
+
+
+TOKENIZER=${CMD}/utf8-tokenize.perl
+TAGGER=${BIN}/tree-tagger
+ABBR_LIST=${LIB}/polish-abbreviations-utf8
+PARFILE=${LIB}/polish-utf8.par
+
+
+$TOKENIZER -a $ABBR_LIST $* |
+# tagging
+$TAGGER $OPTIONS $PARFILE
